09917963ac2cb3ef160bdb3107d8194838341388
Praful Patnecha <prafulpatnecha@gmail.com>
2024-06-26 16:44:22 +0530

    Initial commit

---
+++ lib/utils/globle_list.dart
@@ -0,0 +1,52 @@
+List productDetail=[
+  {
+    "productNo": 1,
+    "category":'pizza',
+    "km":7.5,
+    "openTime":'10:30 AM',//AM/PM
+    "type":'cheese pizza',//small character
+    "closeTime":'7:30 PM',
+    "rating":4,
+    "ratingPerson":104,//1-1000
+    "Time":'35 min',//15-60 min / 1 to 2 hour
+    "price":150,//70-300
+    "productName":"Full Cheese Pizza",//first word also capital word
+    "description":'pizza, dish of Italian origin consisting of a flattened disk of bread dough topped with some combination of olive oil, oregano, tomato, olives, mozzarella',
+    "restaurantName":"Tulsi Restaurant",//first word also capital word
+    "restaurantAddress":"Sardar Vegetable Market, Sanjay Nagar, Surat, Gujarat 395101",//first word also capital word
+    "stateDishes":'gujarat',
+    "quantity":1,//1,2,3
+    "cart":0,//non change
+    "extra":true,//non change
+    "addProduct":false,//non change
+    "favourite":false,//non change
+  }
+];
+//1=large,Medium,small,
+//2=pis
+//3=150 gram/1 kg <- any one
+
+// List productDetail=[
+//   {
+//     "productNo": ,
+//     "category":'',
+//     "km":,
+//     "openTime":'',                                      //AM/PM
+//     "type":'',                                      //small character
+//     "closeTime":'',
+//     "rating":,
+//     "ratingPerson":,                                      //1-1000
+//     "Time":'',                                      //15-60 min / 1 to 2 hour
+//     "price":,                                     //70-300
+//     "productName":"",                                     //first word also capital word
+//     "description":'',
+//     "restaurantName":"",                                      //first word also capital word
+//     "restaurantAddress":"",                                     //first word also capital word
+//     "stateDishes":'',
+//     "quantity":,                                      //1,2,3
+//     "cart":0,//non change
+//     "extra":true,//non change
+//     "addProduct":false,//non change
+//     "favourite":false,//non change
+//   }
+// ];
